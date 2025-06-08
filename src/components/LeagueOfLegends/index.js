fbc2329e4612a28fd4ca502102a0cbde96fa1557
Calvin Tran <calvint05@outlook.com>
2025-06-08 18:47:12 -0500

    Added comments

--- src/components/LeagueOfLegends/index.js
+++ src/components/LeagueOfLegends/index.js
@@ -1,4 +1,22 @@
-// Barrel File for League of Legends components
+/*  
+ * Barrel File for League of Legends components
+ * Imports and exports all League-related components
+ * 
+ * Import methods:
+ * 
+ * - Specify the components you need
+ * import {
+ *      LeagueAccount,
+ *      MasteryCrest,
+ *      RankedEmblem...
+ * } from './components/LeagueOfLegends';
+ * 
+ * - Import all components as a single object
+ * import * as LoL from './components/LeagueOfLegends';
+ * (Access as LoL.LeagueAccount, LoL.MasteryCrest, etc.)
+ * 
+ */
+
 export {default as LeagueAccount} from './LeagueAccount';
 export {default as MasteryCrest} from './MasteryCrest';
 export {default as RankedEmblem} from './RankedEmblem';
