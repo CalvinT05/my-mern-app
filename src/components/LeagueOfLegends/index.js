2fdbba807ff91f9aeede73ee3481a1677bc6e15a
Calvin Tran <calvint05@outlook.com>
2025-06-08 18:35:32 -0500

    Added ranked and mastery display components

---
+++ src/components/LeagueOfLegends/index.js
@@ -0,0 +1,4 @@
+// Barrel File for League of Legends components
+export {default as LeagueAccount} from './LeagueAccount';
+export {default as MasteryCrest} from './MasteryCrest';
+export {default as RankedEmblem} from './RankedEmblem';
