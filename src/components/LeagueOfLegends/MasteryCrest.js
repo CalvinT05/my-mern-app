10ef3864ee62529e71c8bc3cc01bb065e9e71d5b
Calvin Tran <calvint05@outlook.com>
2025-06-09 02:09:05 -0500

    updated icons

--- src/components/LeagueOfLegends/MasteryCrest.js
+++ src/components/LeagueOfLegends/MasteryCrest.js
@@ -9,17 +9,18 @@ import React from 'react';
  * Props: Raw mastery level
  * Renders the mastery crest based on the provided mastery 
  * level
+ * 
  */
 function RankedEmblem({masteryLevelRaw}) {
     // Get the mastery level for the image, defaulting to 0 if less than 4
-    const masteryLevel = (masteryLevelRaw < 4) ? 0 : masteryLevelRaw;
+    const masteryLevel = (masteryLevelRaw > 10) ? 10 : masteryLevelRaw;
     
     // Format the image for display
     return (
       <div>
           <img
-              src={`/league-of-legends/mastery-crests/level-${masteryLevel}.png`}
-              alt={`$Level ${masteryLevel} Crest`}
+              src={`/league-of-legends/mastery-crests/mastery-${masteryLevel}.png`}
+              alt={`Mastery ${masteryLevel} Crest`}
               style={{width: '100px', height: '100px'}}
           />
       </div>
